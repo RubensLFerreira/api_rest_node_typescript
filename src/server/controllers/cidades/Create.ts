30e52194191475cf6d85f8355592cfd5ef9bbef1
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-23 12:09:07 -0300

    fazendo validação com yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from 'express';
+// import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 interface ICidade { // tipando dado
   nome: string;
 }
 
-export const Create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  // O dado passado no req é o dado passando na interface
-  console.log(req.body);
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+
+export const Create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body);
+    // recebendo os dados validados
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message
+      }
+    });
+  }
+
+  console.log(validatedData);
 
   return res.send('Create');
 }; 
