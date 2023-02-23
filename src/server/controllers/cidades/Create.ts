d55a02a7570bc8fd10cb447f77eb137b8e6e9147
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-23 16:03:50 -0300

    validações de vários erros

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,10 +3,12 @@ import { Request, Response } from 'express';
 import * as yup from 'yup';
 interface ICidade { // tipando dado
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 
@@ -14,16 +16,19 @@ export const Create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false});
     // recebendo os dados validados
   } catch (error) {
     const yupError = error as yup.ValidationError;
+    const errors: Record<string, string> = { };
 
-    return res.json({
-      errors: {
-        default: yupError.message
-      }
+    yupError.inner.forEach(error => {
+      if (!error.path) return;
+
+      errors[error.path] = error.message;
     });
+
+    return res.json({ errors });
   }
 
   console.log(validatedData);
