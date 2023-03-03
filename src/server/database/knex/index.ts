eca7b6f251de071c20d5acc928568ab5449a60e6
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-03-03 19:42:16 -0300

    configuracao inicial knex

---
+++ src/server/database/knex/index.ts
@@ -0,0 +1,13 @@
+import knex from 'knex';
+import {development, test, production} from './Enviroment';
+
+const getEnviroment = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production': return production;
+    case 'test': return test;
+
+    default: return development;
+  }
+};
+
+export const Knex = knex(getEnviroment());
