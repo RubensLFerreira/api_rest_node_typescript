f9f07082e904da9d2da7374419862ce86823ce74
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-03-07 12:05:04 -0300

    criando provider deleteById

--- src/server/controllers/cidades/DeleteById.ts
+++ src/server/controllers/cidades/DeleteById.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+
 import * as yup from 'yup';
 
 import { validation } from '../../shared/middleware';
+import { CidadesProvider } from '../../providers/cidades';
 
 interface IParamsProps {
   id?: number;
@@ -16,11 +18,23 @@ export const deleteByIdValidation = validation((getSchema) => ({
 
 export const deleteById = async (req: Request<IParamsProps>, res: Response) => {
 
-  if(Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    errors: {
-      default: 'Registro não encontrado',
-    }
-  });
+  if(!req.params.id) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: 'Nenhum ID foi informado!', 
+      }
+    });
+  }
+
+  const result = await CidadesProvider.deleteById(req.params.id);
+
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  }
 
   return res.status(StatusCodes.NO_CONTENT).send();
 };
