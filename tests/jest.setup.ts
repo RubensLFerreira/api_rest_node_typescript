507240199f4451c8fd54954ea5cbff267e7b41f3
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-03-07 11:01:31 -0300

    knex em memoria para jest

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,16 @@
 import supertest from 'supertest';
 
 import { server } from '../src/server/Server';
+import { Knex } from '../src/server/database/knex';
+
+// Antes do teste espere as migrations
+beforeAll( async () => {
+  await Knex.migrate.latest();
+});
+
+// Desconecta do Knex após o migrations
+afterAll( async () => {
+  await Knex.destroy();
+});
 
 export const testServer = supertest(server);
