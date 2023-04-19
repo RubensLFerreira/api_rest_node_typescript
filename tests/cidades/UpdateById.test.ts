027c61a2e15d47806f01969a25200bea951ea090
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-04-19 18:02:48 -0300

    Alterando teste de cidade

--- tests/cidades/UpdateById.test.ts
+++ tests/cidades/UpdateById.test.ts
@@ -5,21 +5,22 @@ describe('Cidades - UpdateById', () => {
   it('Atualizar registro', async () => {
     const res1 = await testServer
       .post('/cidades')
-      .send({ nome: 'Springfield' });
+      .send({ nome: 'cedro'});
 
     expect(res1.statusCode).toEqual(StatusCodes.CREATED);
 
-    const resUpdate = await testServer
+    const resAtualizar = await testServer
       .put(`/cidades/${res1.body}`)
-      .send({ nome: 'Cedro' });
+      .send({ nome: 'Iguatu'});
 
-    expect(resUpdate.status).toEqual(StatusCodes.NO_CONTENT);
+    expect(resAtualizar.statusCode).toEqual(StatusCodes.NO_CONTENT);
   });
 
   it('Tentar atualizar registro inexistente', async () => {
     const res1 = await testServer
       .put('/cidades/99999')
       .send({ nome: 'Cedro'});
+
     expect(res1.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(res1.body).toHaveProperty('errors.default');
   });
